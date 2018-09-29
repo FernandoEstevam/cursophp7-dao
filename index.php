6d0a1b91754037ee9ed1a24c87bd5bfc4476a5a8
Fernando Estevam <fernandoestevam23@gmail.com>
2018-09-29 15:25:16 -0300

    Iniciando arquivo index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    //Chamando o autoload
+    require_once("config.php");
+
+    $sql = new Sql();
+
+    $usuarios = $sql->select("SELECT * FROM tb_usuarios");
+
+    echo json_encode($usuarios);
+?>
